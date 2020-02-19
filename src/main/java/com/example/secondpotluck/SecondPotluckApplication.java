4dbd6c6959a7dc9f034e2d3cf443aeefd027fc3b
GBTC440017UR <GBTC440017UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-19 11:51:53 -0500

    First Commit

---
+++ src/main/java/com/example/secondpotluck/SecondPotluckApplication.java
@@ -0,0 +1,13 @@
+package com.example.secondpotluck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondPotluckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondPotluckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/secondpotluck/SecondPotluckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secondpotluck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondPotluckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
